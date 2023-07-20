50e9f841c596ea5a1fc2d4c87fc78ad48c20b718
Dominik <44637946+dominikzorgnotti@users.noreply.github.com>
2023-07-20 12:13:53 +0200

    changed instance type to 2 vcpu

--- bash/cka/prep-gcp-for-cka.sh
+++ bash/cka/prep-gcp-for-cka.sh
@@ -17,5 +17,4 @@ gcloud storage cp cka-k8s-basic-setup-ubuntu2004.sh gs://bucket-cka-prep-01/cka-
 gcloud services enable
 
 ## Create an instance template
-gcloud compute instance-templates create it-cka-prep-v01 --machine-type=t2d-standard-1 --metadata=startup-script-url=gs://bucket-cka-prep-01/cka-k8s-basic-setup-ubuntu2004.sh --scopes=https://www.googleapis.com/auth/cloud-platform --tags=k8s --create-disk=auto-delete=yes,boot=yes,device-name=it-cka-prep-v01,image=projects/ubuntu-os-cloud/global/images/ubuntu-2004-focal-v20230715,mode=rw,size=40,type=pd-balanced
-
+gcloud compute instance-templates create it-cka-prep-v01 --machine-type=t2d-standard-2 --metadata=startup-script-url=gs://bucket-cka-prep-01/cka-k8s-basic-setup-ubuntu2004.sh --scopes=https://www.googleapis.com/auth/cloud-platform --tags=k8s --create-disk=auto-delete=yes,boot=yes,device-name=it-cka-prep-v01,image=projects/ubuntu-os-cloud/global/images/ubuntu-2004-focal-v20230715,mode=rw,size=40,type=pd-balanced
