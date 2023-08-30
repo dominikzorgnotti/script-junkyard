5455c218e82ef1d27e5f845d53d0f39a37778fb8
Dominik <44637946+dominikzorgnotti@users.noreply.github.com>
2023-08-30 17:54:49 +0200

    Update get-num-cpu-cores.sh
    Alternative way

--- govc/get-num-cpu-cores.sh
+++ govc/get-num-cpu-cores.sh
@@ -1,3 +1,7 @@
 # A quick one-liner to get all the CPU cores (Packages multiplied by number of cores) of all ESXi hosts in a vCenter
 
 govc object.collect -s -type HostSystem -json - hardware | jq '[.ChangeSet[].Val | .SystemInfo.Vendor, .SystemInfo.Model, .CpuInfo.NumCpuPackages, .CpuInfo.NumCpuCores, (.CpuInfo.NumCpuPackages)*(.CpuInfo.NumCpuCores)] '
+
+# This just prints out the total sum of all CPU cores
+
+govc object.collect -s -type HostSystem -json - hardware | jq '(.ChangeSet[].Val.CpuInfo.NumCpuPackages * .ChangeSet[].Val.CpuInfo.NumCpuCores)' | jq -s 'add'
