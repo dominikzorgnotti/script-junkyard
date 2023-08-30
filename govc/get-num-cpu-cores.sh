d577a9a46015b66726ea12246ac6b88fd1137d38
Dominik <44637946+dominikzorgnotti@users.noreply.github.com>
2023-08-30 18:27:56 +0200

    Update get-num-cpu-cores.sh

--- govc/get-num-cpu-cores.sh
+++ govc/get-num-cpu-cores.sh
@@ -1,3 +1,9 @@
+
+# This just prints out the total sum of all CPU core license packs, see https://kb.vmware.com/s/article/89116
+
+govc object.collect -s -type HostSystem -json - hardware | jq -r '{Sockets: .ChangeSet[].Val.CpuInfo.NumCpuPackages, CoresPerSocket: .ChangeSet[].Val.CpuInfo.NumCpuCores, LicensePacks: ((.ChangeSet[].Val.CpuInfo.NumCpuCores / 16 | ceil ) * .ChangeSet[].Val.CpuInfo.NumCpuPackages)}' | jq -s '[.[].LicensePacks] | add'
+
+
 # A quick one-liner to get all the CPU cores (Packages multiplied by number of cores) of all ESXi hosts in a vCenter
 
 govc object.collect -s -type HostSystem -json - hardware | jq '[.ChangeSet[].Val | .SystemInfo.Vendor, .SystemInfo.Model, .CpuInfo.NumCpuPackages, .CpuInfo.NumCpuCores, (.CpuInfo.NumCpuPackages)*(.CpuInfo.NumCpuCores)] '
