31f1c680775592ed9b831091b21dbef709a8cc0e
Dominik <44637946+dominikzorgnotti@users.noreply.github.com>
2023-12-18 12:21:30 +0100

    Update get-num-cpu-cores.sh
    simple cpu socket counter

--- govc/get-num-cpu-cores.sh
+++ govc/get-num-cpu-cores.sh
@@ -1,6 +1,9 @@
 
 # This just prints out the total sum of all CPU core license packs, see https://kb.vmware.com/s/article/89116
 
-# Updated for breaking changes in GOVC, now the JSON keys are in camelCase.
+# Updated for breaking changes in GOVC. Since 0.32 the JSON keys are in camelCase.
 govc object.collect -s -type HostSystem -json - hardware | jq -r '{Sockets: .changeSet[].val.cpuInfo.numCpuPackages, CoresPerSocket: .changeSet[].val.cpuInfo.numCpuCores, LicensePacks: ((.changeSet[].val.cpuInfo.numCpuCores / 16 | ceil ) * .changeSet[].val.cpuInfo.numCpuPackages)}' | jq -s '[.[].LicensePacks] | add'
+
+# Only the sockets
+govc object.collect -json -type h / hardware.cpuInfo.numCpuPackages | jq -r '.changeSet[].val' | jq -s 'add'
 
