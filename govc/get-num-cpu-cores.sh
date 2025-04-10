5545463af54c00e2bac6e2680dd394089a56faba
Dominik <44637946+dominikzorgnotti@users.noreply.github.com>
2025-04-10 12:14:33 +0200

    Update get-num-cpu-cores.sh

--- govc/get-num-cpu-cores.sh
+++ govc/get-num-cpu-cores.sh
@@ -4,6 +4,9 @@
 # Updated for breaking changes in GOVC. Since 0.32 the JSON keys are in camelCase.
 govc object.collect -s -type HostSystem -json - hardware | jq -r '{Sockets: .changeSet[].val.cpuInfo.numCpuPackages, CoresPerSocket: .changeSet[].val.cpuInfo.numCpuCores, LicensePacks: ((.changeSet[].val.cpuInfo.numCpuCores / 16 | ceil ) * .changeSet[].val.cpuInfo.numCpuPackages)}' | jq -s '[.[].LicensePacks] | add'
 
+# Now everything is Starting capital
+govc object.collect -s -type HostSystem -json - hardware | jq -r '{Sockets: .ChangeSet[].Val.CpuInfo.NumCpuPackages, CoresPerSocket: .ChangeSet[].Val.CpuInfo.NumCpuCores, LicensePacks: ((.ChangeSet[].Val.CpuInfo.NumCpuCores / 16 | ceil ) * .ChangeSet[].Val.CpuInfo.NumCpuPackages)}' | jq -s '[.[].LicensePacks] | add'
+
 # Only the sockets
 govc object.collect -json -type h / hardware.cpuInfo.numCpuPackages | jq -r '.changeSet[].val' | jq -s 'add'
 
